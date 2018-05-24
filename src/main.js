15f97a3611fe21f112f5c9a8b8684e8c8925c732
Eva Antipina <eva.antipina@gmail.com>
2018-05-23 22:59:17 -0700

    Finish UI logic and add styles.

--- src/main.js
+++ src/main.js
@@ -7,10 +7,31 @@ import { Haiku } from './haiku.js';
 $(document).ready(function() {
   $('#haiku-form').submit(function(event) {
     event.preventDefault();
+
     let input1 = $('#first-line').val().split(/[\s\.:;?!~,`"&|\(\)<>{}\[\]\r\n/\\]+/).filter(n => n);
     let input2 = $('#second-line').val().split(/[\s\.:;?!~,`"&|\(\)<>{}\[\]\r\n/\\]+/).filter(n => n);
     let input3 = $('#third-line').val().split(/[\s\.:;?!~,`"&|\(\)<>{}\[\]\r\n/\\]+/).filter(n => n);
 
-    Haiku.countSyllables(input);
+    let syllables1 = 0;
+    input1.forEach(function(word){
+      syllables1 += Haiku.countSyllables(word);
+      console.log(syllables1);
+    });
+    let syllables2 = 0;
+    input2.forEach(function(word){
+      syllables2 += Haiku.countSyllables(word);
+    });
+    let syllables3 = 0;
+    input3.forEach(function(word){
+      syllables3 += Haiku.countSyllables(word);
+    });
+
+    if (syllables1 === 5 && syllables2 === 7 && syllables3 === 5) {
+      $("#checker-output").text("Congratulations! Your poem is a true haiku!");
+    } else {
+      $("#checker-output").text("Sorry, your poem is not a haiku!");
+    }
+    $("#output").show();
+
   });
 });
